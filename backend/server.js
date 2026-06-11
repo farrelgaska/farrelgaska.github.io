5ab7f6e7b8f2bbd9d3a9d063a9115826f277f3f4
Farrel Gaska Haryadi <farrelgaskaharyadi@gmail.com>
2026-06-12 03:16:57 +0700

    update backend url render

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const fs = require("fs");
 const path = require("path");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const DATA_DIR = path.join(__dirname, "data");
 const UPLOAD_DIR = path.join(__dirname, "uploads");
